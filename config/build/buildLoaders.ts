86ff19915ffd939d5b975a2e81279259871f4aa0
Maria Chuikina <4uikina@gmail.com>
2023-02-02 15:13:14 +0100

    1.4v scss-loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,17 @@ import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			"style-loader",
+			"css-loader",
+			"sass-loader"
+		],
+	}
+
+
+
 	const typescriptLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
@@ -11,5 +22,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
 	return [
 			typescriptLoader,
+			cssLoader
 		]
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from './test';
-
-someFn(123);
-
-document.body.innerHTML = `<div style = "border: 1px solid teal">HELLO WORLD!</div> `
