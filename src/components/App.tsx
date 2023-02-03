06a1656eeabbebe623e74854aa807f77beac9753
Maria Chuikina <4uikina@gmail.com>
2023-02-03 11:34:25 +0100

    1.5v css-modules + css

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { Counter } from './Counter'
+import './index.scss'
+
+const App = () => {
+	return (
+		<div className='app'>
+			<div>Some Text</div>
+			<Counter />
+		</div>
+
+	)
+}
+
+export default App;
