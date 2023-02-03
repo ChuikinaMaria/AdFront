c794eed8d42ec1aca15bed5983b815ac2539c3ed
Maria Chuikina <4uikina@gmail.com>
2023-02-03 14:18:37 +0100

    1.6v  routing Lazy Suspence

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,20 @@
-import { Counter } from './Counter'
 import './index.scss'
+import { Route, Routes, Link } from 'react-router-dom';
+import { MainPageAsync } from '../pages/MainPage/MainPageAsync';
+import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 const App = () => {
 	return (
 		<div className='app'>
-			<div>Some Text</div>
-			<Counter />
+			<Link to='/'>MAIN PAGE</Link>
+			<Link to='/about'>ABOUT PAGE</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<MainPageAsync />}/>
+					<Route path={'/about'} element={<AboutPageAsync />} />
+				</Routes>
+			</Suspense>
 		</div>
 
 	)
