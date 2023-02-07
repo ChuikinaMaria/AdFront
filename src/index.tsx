ff62d8ed2fab34870ad7a33505e6bda1305afb19
Maria Chuikina <4uikina@gmail.com>
2023-02-07 11:14:36 +0100

    varScss useTheme localstorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import ThemeProvider from './styles/theme/ThemeProvider';
 
 
 
 render(
 	<BrowserRouter>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</BrowserRouter>,
 	document.getElementById("root")
 )
