f00efd81801f518260934818e81cf7cd29cb6e7f
Maria Chuikina <4uikina@gmail.com>
2023-02-07 11:14:36 +0100

    1.7v var-scss useTheme localstorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
-import './index.scss'
+import '../styles/index.scss'
 import { Route, Routes, Link } from 'react-router-dom';
 import { MainPageAsync } from '../pages/MainPage/MainPageAsync';
 import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from 'react';
+import { ThemeContext } from '../styles/theme/ThemeContext';
+import { Theme } from '../styles/theme/ThemeContext';
+import { useTheme } from '../styles/theme/useTheme';
+
+
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme();	
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+
+			<button onClick={toggleTheme}>TOGGLE</button>
 			<Link to='/'>MAIN PAGE</Link>
 			<Link to='/about'>ABOUT PAGE</Link>
 			<Suspense fallback={<div>Loading...</div>}>
